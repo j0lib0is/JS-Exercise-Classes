a5319bb8c107c6f6c42471de55fbd3a7f4023bf3
jacobjolibois <jacobjolibois@hey.com>
2021-11-11 10:45:10 -0600

    Completed tasks 1 and 2

--- index.js
+++ index.js
@@ -8,6 +8,8 @@
         + If a plane lands, its `isFlying` property gets set to false.
 */
 
+const { enumDeclaration } = require("@babel/types");
+
 // EXAMPLE SOLUTION CODE:
 class Airplane {
   constructor(name) {
@@ -31,41 +33,89 @@ class Airplane {
 
 
 /*
-  TASK 1
-    - Write a Person class whose constructor initializes `name` and `age` from arguments.
-    - All instances of Person should also initialize with an empty `stomach` array.
-    - Give instances of Person the ability to `.eat("someFood")`:
-        + When eating an edible, it should be pushed into the `stomach`.
-        + The `eat` method should have no effect if there are 10 items in the `stomach`.
-    - Give instances of Person the ability to `.poop()`:
-        + When an instance poops, its `stomach` should empty.
-    - Give instances of Person a method `.toString()`:
-        + It should return a string with `name` and `age`. Example: "Mary, 50"
+  ✅ TASK 1
+    - ✅ Write a Person class whose constructor initializes `name` and `age` from arguments.
+    - ✅ All instances of Person should also initialize with an empty `stomach` array.
+    - ✅ Give instances of Person the ability to `.eat("someFood")`:
+        + ✅ When eating an edible, it should be pushed into the `stomach`.
+        + ✅ The `eat` method should have no effect if there are 10 items in the `stomach`.
+    - ✅ Give instances of Person the ability to `.poop()`:
+        + ✅ When an instance poops, its `stomach` should empty.
+    - ✅ Give instances of Person a method `.toString()`:
+        + ✅ It should return a string with `name` and `age`. Example: "Mary, 50"
 */
 
 class Person {
-  
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+    this.stomach = [];
+  }
+  eat(someFood) {
+    if (this.stomach.length < 10) {
+      this.stomach.push(someFood);
+    }
+  }
+  poop() {
+    this.stomach = [];
+  }
+  toString() {
+    return `${this.name}, ${this.age}`;
+  }
 }
+
+// TEST
+// const john = new Person('John', '100');
+// john.eat('pizza');
+// john.eat('cake');
+// console.log(john.stomach);
+// john.poop();
+// console.log(john.stomach);
 
 
 
 /*
   TASK 2
-    - Write a Car class whose constructor initializes `model` and `milesPerGallon` from arguments.
-    - All instances built with Car:
-        + should initialize with a `tank` at 0
-        + should initialize with an `odometer` at 0
-    - Give cars the ability to get fueled with a `.fill(gallons)` method. Add the gallons to `tank`.
-    - Give cars ability to `.drive(distance)`. The distance driven:
-        + Should cause the `odometer` to go up.
-        + Should cause the the `tank` to go down taking `milesPerGallon` into account.
-    - A car which runs out of `fuel` while driving can't drive any more distance:
-        + The `drive` method should return a string "I ran out of fuel at x miles!" x being `odometer`.
+    - ✅ Write a Car class whose constructor initializes `model` and `milesPerGallon` from arguments.
+    - ✅ All instances built with Car:
+        + ✅ should initialize with a `tank` at 0
+        + ✅ should initialize with an `odometer` at 0
+    - ✅ Give cars the ability to get fueled with a `.fill(gallons)` method. Add the gallons to `tank`.
+    - ✅ Give cars ability to `.drive(distance)`. The distance driven:
+        + ✅ Should cause the `odometer` to go up.
+        + ✅ Should cause the the `tank` to go down taking `milesPerGallon` into account.
+    - ✅ A car which runs out of `fuel` while driving can't drive any more distance:
+        + ✅ The `drive` method should return a string "I ran out of fuel at x miles!" x being `odometer`.
 */
 
 class Car {
-  
+  constructor(model, milesPerGallon) {
+    this.model = model;
+    this.milesPerGallon = milesPerGallon;
+    this.tank = 0;
+    this.odometer = 0;
+  }
+  fill(gallons) {
+    this.tank += gallons;
+  }
+  drive(distance) {
+    if (this.tank * this.milesPerGallon < distance) {
+      const milesDriven = this.tank * this.milesPerGallon;
+      this.odometer += milesDriven;
+      this.tank = 0;
+      return `I ran out of fuel at ${this.odometer} miles!`;
+    } else {
+      this.odometer += distance;
+      this.tank = distance / this.milesPerGallon;
+    }
+  }
 }
+
+// TEST
+// const myCar = new Car('Tacoma', 24);
+// myCar.fill(5);
+// console.log(myCar.tank);
+// console.log(myCar.drive(130));
 
 
 
